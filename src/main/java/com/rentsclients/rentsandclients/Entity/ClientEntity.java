e206da187a2a66b844c89c2fc905f618aa957e64
admin.ian.swerts <iansw.stank@gmail.com>
2025-02-27 19:46:38 +0000

    Some Tests in client

--- src/main/java/com/rentsclients/rentsandclients/Entity/ClientEntity.java
+++ src/main/java/com/rentsclients/rentsandclients/Entity/ClientEntity.java
@@ -5,11 +5,12 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
 
 import lombok.Setter;
 
-
+@Builder
 @Getter
 @Setter
 @Entity(name = "client")
