9b389a47204727a7aff0349bdac5a9aac9c13ae5
admin.ian.swerts <iansw.stank@gmail.com>
2025-02-27 08:04:39 +0000

    binding implemented in Client

--- src/main/java/com/rentsclients/rentsandclients/Entity/ClientEntity.java
+++ src/main/java/com/rentsclients/rentsandclients/Entity/ClientEntity.java
@@ -1,9 +1,14 @@
 package com.rentsclients.rentsandclients.Entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 import lombok.Setter;
+
 
 @Getter
 @Setter
@@ -12,9 +17,17 @@ public class ClientEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long clientid;
+    @Column(nullable = false)
+    @Size(min = 3, max = 50, message = "The client first name do not approved")
+    @NotBlank(message = "The client firstName is empty")
     private String firstName;
+    @Column(nullable = false)
+    @NotBlank(message = "The client lastName is empty")
+    @Size(min = 3, max = 50, message = "The client last name do not approved")
     private String lastName;
     @Column(unique = true, nullable = false)
+    @Min(value = 100000000, message = "The clientNif do not approved")
+    @Max(999999999)
     private int nif;
     private boolean activated;
 
